c216fb0ff6dee4cf3a66746fee10f8176fc26d6f
shiraiwa <shiraiwa@psfc.mit.edu>
2017-02-16 20:04:38 -0500

    property editer is added to hdf export window

---
+++ python/ifigure/mto/metadata.py
@@ -0,0 +1,112 @@
+from collections import OrderedDict
+import six
+
+# this is assigned to figplot
+default_metadata_keys = ('name',
+                         'long_description')
+
+# this is assigned to figpage
+default_metadata_file_keys = ('name',
+                                   'long_description',
+                                   'source', 
+                                   'date',
+                                   'user_name',
+                                   'user_id',
+                                   'comment',
+                                   'keywords')
+
+# this is assigned to figplot under data dictionary key.
+default_metadata_data_keys = ('name',
+                                   'long_description',
+                                   'units',
+                                   'range')
+# In addition to this shape and type will be written 
+# automatically.
+
+class MetadataHolder(object):
+    def set_default_metadata(self):
+        return self.setvar('metadata', OrderedDict())
+
+    def has_metadata(self):
+        return self.getvar('metadata') is not None
+    def fill_default_metadatavalue(self):
+        pass
+
+    def get_metadata(self):
+        return self.getvar('metadata')
+
+    def set_metadata(self, *args):
+        '''
+        set_metadata({'xdata':'{'name':'',..
+                      'ydata':'{'name':''...})
+        set_metadata('x_axis', 'xlabel')
+        '''
+        if len(args) == 1:
+           self.setvar('metadata', args[0])
+        elif len(args) == 2:
+           if self.getvar('metadata') is None:
+               self.set_default_metadata()
+           self.getvar('metadata')[args[0]] = args[1]
+    
+    def update_data_metadata(self, updatename = False):
+        # autofill name,  range
+        # if data is not give, try to get it
+        metadata = self.getvar('metadata')
+        for k in six.iterkeys(metadata):
+            if k.startswith('data'):
+                dd = metadata[k]
+                for name in six.iterkeys(dd):
+                    axisname = name[0] # x, y, z, c
+                    m = 'get_'+axisname+'axisparam'
+                    if hasattr(self, m):
+                       m = getattr(self, m)
+                       param =m()
+                       if updatename: 
+                           dd[name]['name']=param.labelinfo[0]
+                       dd[name]['range'] = str(param.range)
+
+    def update_file_metadata(self):
+        from ifigure.mto.fig_page import FigPage
+        if not isinstance(self, FigPage): return
+        from ifigure.utils.get_username import get_username
+        self.getvar('metadata')['user_id'] = get_username()
+
+    def assign_default_file_metadata(self, data=None):
+        from ifigure.mto.fig_page import FigPage
+        if not isinstance(self, FigPage): return
+        if self.has_metadata(): return
+        meta =OrderedDict()
+        for k in default_metadata_file_keys:
+            meta[k] = ''
+        self.setvar('metadata', meta)
+        self.update_file_metadata()
+
+    def assign_default_metadata(self, data=None):
+        '''
+        will be overwritten by subclass
+        '''
+        def fill_dataset(dd, d):
+            for key in six.iterkeys(d):
+                dd[key] = OrderedDict()
+                for kk in default_metadata_data_keys:
+                    dd[key][kk] = ''
+        if self.has_metadata(): return
+        metadata = OrderedDict()
+        self.setvar('metadata', metadata)
+        for key in default_metadata_keys:
+            metadata[key] = ''
+        if data is None:
+            try:
+                data = self.export()
+            except:
+                pass
+        if data is not None:
+           if len(data) == 1:
+               metadata['data'] = {}
+               fill_dataset(metadata['data'], data[0])         
+           else:
+               for kk,  ddd in enumerate(data):
+                   metadata['data'+str(kk+1)] = {}
+                   fill_dataset(metadata['data'+str(kk+1)],
+                                ddd)
+        self.update_data_metadata(updatename = True)
