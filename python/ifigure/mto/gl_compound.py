5202ea6cdc6fda3ef62c38b98f4a5c0b58c69fe1
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-03-11 13:49:52 -0400

    minor fix

--- python/ifigure/mto/gl_compound.py
+++ python/ifigure/mto/gl_compound.py
@@ -81,8 +81,9 @@ class GLCompound(object):
         return self._artists[0]._gl_hit_array_id
 
     def setSelectedIndex(self, ll):
-        array_idx = self.getvar('array_idx').copy()
+        array_idx = self.getvar('array_idx')
         if array_idx is None: return
+        array_idx = array_idx.copy()
         mask = np.isin(array_idx, np.array(ll, copy=False))
         array_idx[mask] *= -1
         for a in self._artists:                  
