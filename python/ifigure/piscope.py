5e94071dc77187f434a74c9cabceaefac88f72fb
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-02-19 15:08:48 -0500

    added __main__.py

--- python/ifigure/piscope.py
+++ python/ifigure/piscope.py
@@ -1,9 +1,5 @@
-from __future__ import print_function
-import multiprocessing as mp
-#import sys, os, wx, weakref
-#sys.path = [os.path.dirname(os.path.dirname(__file__)),] + sys.path
-
-if __name__ == '__main__':
+def piscope();
+    import multiprocessing as mp
     import pkg_resources  # somehow importing this later cause warning message
     import mpl_toolkits
     import sys
