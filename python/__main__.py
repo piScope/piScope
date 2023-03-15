70240093f1b2945a9627e659991fec7ded270e72
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2023-03-14 23:42:14 -0400

    minor edit

--- python/__main__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import multiprocessing as mp
-#import sys, os, wx, weakref
-#sys.path = [os.path.dirname(os.path.dirname(__file__)),] + sys.path
-
-if __name__ == '__main__':
-    from .piscope import piscope
-    piscope()
