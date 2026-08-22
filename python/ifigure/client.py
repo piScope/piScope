288e55ea08d79580a25a31ead8f581fceba4379a
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2026-08-22 16:17:02 -0400

    added clean up / PromptTracker / covered all interactive command in client mode

--- python/ifigure/client.py
+++ python/ifigure/client.py
@@ -19,7 +19,7 @@ from __future__ import print_function
         ## here a user export plot data to piScope shell, and
         ## create xdata and ydata
 
-        print xdata     # in server/client mode, the data is automatically 
+        print xdata     # in server/client mode, the data is automatically
                         # sent
 
         ## a user can also get any pickable variables in piScope shell
@@ -36,10 +36,54 @@ import binascii
 import threading
 import os
 import queue
+import signal
+import readline
 
 from six.moves import socketserver
 from ifigure.utils.cbook import pick_unused_port
 
+
+# async_print
+#
+#  show message in Python interpromt w/o destroying what is shown now.
+#  currnet_prompt stores the last prompt used by Interpreter
+
+current_prompt = ">>> "
+
+class TrackingPrompt:
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        global current_prompt
+        current_prompt = self.text
+        return self.text
+
+sys.ps1 = TrackingPrompt(sys.ps1)
+sys.ps2 = TrackingPrompt(sys.ps2)
+
+def async_print(*args, **kwargs):
+    """
+    Print without permanently destroying the line currently being typed
+    at the interactive prompt.
+    """
+    # Save current typed text.
+    buf = readline.get_line_buffer()
+
+    # Move to beginning of line and clear it.
+    sys.stdout.write("\r\033[2K")
+    sys.stdout.flush()
+
+    print(*args, **kwargs)
+
+    # Redraw prompt + current input.
+    sys.stdout.write(current_prompt + buf)
+    sys.stdout.flush()
+
+#
+#  ReceiverReqHandler : receive data from piscope and send message to main thread
+#  using signal
+#
 
 class ReceiverReqHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -49,7 +93,7 @@ class ReceiverReqHandler(socketserver.BaseRequestHandler):
         data = cPickle.loads(binascii.a2b_hex(response))
         if data['type'] == 'data':
             import __main__
-            text = ''
+            text = '\n'
             for key in data['data'][0]:
                 if key in dir(__main__):
                     text = text + key + ' is updated. \n'
@@ -61,9 +105,9 @@ class ReceiverReqHandler(socketserver.BaseRequestHandler):
             self.server.msg_queue.put((data['data']))
         else:
             self.server.msg_queue.put("")
+        os.kill(os.getpid(), signal.SIGUSR1)
 
 class Receiver(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.msg_queue = queue.Queue()
@@ -111,10 +155,23 @@ class Client(object):
                 target=Client.receiver.serve_forever)
             server_thread.daemon = True
             server_thread.start()
+
+        signal.signal(signal.SIGUSR1, self.signal_handler)
+
         ip, port = Client.receiver.server_address
         print(('receiver :', ip, ':', port))
         message = cPickle.dumps(('r', ip, port))
         self.send(message, noresponse=True)
+
+    def signal_handler(self, signum, frame):
+        # This handler executes in Python's main thread.
+        while True:
+            try:
+                msg = Client.receiver.msg_queue.get_nowait()
+                async_print(msg)
+                Client.receiver.msg_queue.task_done()
+            except queue.Empty:
+                break
 
     def shutdown(self):
         if Client.process is not None:
@@ -148,13 +205,7 @@ class Client(object):
         finally:
             sock.close()
 
-        if not noresponse:
-           msg = Client.receiver.msg_queue.get()
-           print(msg)
-           Client.receiver.msg_queue.task_done()
-        
         return response
-
 
 def launch(exe=None):
     server('launch', exe=exe)
@@ -163,10 +214,12 @@ def launch(exe=None):
 def shutdown():
     server('shutdown')
 
+import atexit
+atexit.register(shutdown)
 
 def server(param, host='localhost', port=None, exe=None):
     '''
-    launch/connect piscope 
+    launch/connect piscope
     server('coonect', host, port)
     server('launch')
     server('shutdown')
@@ -247,6 +300,53 @@ def _send_message_g(command, *args, **kargs):
     return c.send(message)
 
 
+names = ['figure',
+         'showpage', 'cla', 'cls', 'clf', 'nsec', 'nsection',
+         'subplot', 'isec', 'isection', 'addpage', 'delpage',
+         'suptitle', 'title',
+         'xlabel', 'xtitle', 'ylabel', 'ytitle', 'zlabel', 'ztitle',
+         'clabel', 'ctitle',
+         'xlog', 'ylog', 'clog', 'zlog',
+         'xsymlog', 'ysymlog', 'zsymlog', 'csymlog',
+         'xlinear', 'ylinear', 'clinear', 'zlinear',
+         'xauto', 'yauto', 'zauto', 'cauto',
+         'xlim', 'ylim', 'zlim', 'clim',
+         'twinx', 'twiny',
+         'oplot', 'oerrorbar',
+         'loglog', 'semilogy', 'semilogx',
+         'timetrace', 'plotc', 'errorbarc',
+         'plot', 'scatter', 'hist', 'triplot', 'errorbar', 'annotate',
+         'ispline', 'contour', 'contourf', 'quiver', 'quiver3d',
+         'image', 'specgram', 'spec', 'tripcolor', 'tricontour',
+         'tricontourf', 'axline', 'axlinec', 'axspan', 'axspanc',
+         'text', 'figtext', 'arrow', 'figarrow', 'legend', 'fill',
+         'fill_between', 'fill_betweenx', 'fill_between_3d', 'surf',
+         'surface', 'revolve', 'solid', 'trisurf', 'property', 'threed',
+         'lighting','view',
+         'xnames', 'ynames', 'znames', 'cnames',
+         'cbar', 'savefig', 'savedata',
+         ]
+
+for name in names:
+    def f(*args, _name=name,  **kargs):
+        print("_sending messsage", _name)
+        _send_message(_name, *args, **kargs)
+    globals()[name] = f
+
+def get(*args, **kargs):
+    return _send_message_g('get_shellvar', *args, **kargs)
+
+
+def put(*args, **kargs):
+    return _send_message_g('pet_shellvar', *args, **kargs)
+
+def plot(*args, **kargs):
+    _send_message('plot', *args, **kargs)
+
+# launch piScope whne from ifigure.client import * is called.
+launch()
+
+'''
 def cla(*args, **kargs):
     _send_message('cla', *args, **kargs)
 
@@ -310,14 +410,73 @@ def xlabel(*args, **kargs):
 def ylabel(*args, **kargs):
     _send_message('ylabel', *args, **kargs)
 
+def zlabel(*args, **kargs):
+    _send_message('zlabel', *args, **kargs)
+
+def clabel(*args, **kargs):
+    _send_message('clabel', *args, **kargs)
+
+xtitle = xlabel
+ytitle = ylabel
+ztitle = zlabel
+ctitle = clabel
+
+def xlog(*args, **kargs):
+    _send_message('xlog', *args, **kargs)
+
+def ylog(*args, **kargs):
+    _send_message('ylog', *args, **kargs)
+
+def zlog(*args, **kargs):
+    _send_message('zlog', *args, **kargs)
+
+def xsymlog(*args, **kargs):
+    _send_message('xsymlog', *args, **kargs)
+
+def ysymlog(*args, **kargs):
+    _send_message('ysymlog', *args, **kargs)
+
+def zsymlog(*args, **kargs):
+    _send_message('zsymlog', *args, **kargs)
+
+def csymlog(*args, **kargs):
+    _send_message('csymlog', *args, **kargs)
+
+def xlinear(*args, **kargs):
+    _send_message('xlinear', *args, **kargs)
+
+def ylinear(*args, **kargs):
+    _send_message('ylinear', *args, **kargs)
+
+def zlinear(*args, **kargs):
+    _send_message('zlinear', *args, **kargs)
+
+def clinear(*args, **kargs):
+    _send_message('clinear', *args, **kargs)
+
+def xatuo(*args, **kargs):
+    _send_message('xauto', *args, **kargs)
+
+def yauto(*args, **kargs):
+    _send_message('yauto', *args, **kargs)
+
+def zauto(*args, **kargs):
+    _send_message('zauto', *args, **kargs)
+
+def cauto(*args, **kargs):
+    _send_message('cauto', *args, **kargs)
 
 def xlim(*args, **kargs):
     _send_message('xlim', *args, **kargs)
 
-
 def ylim(*args, **kargs):
     _send_message('ylim', *args, **kargs)
 
+def zlim(*args, **kargs):
+    _send_message('zlim', *args, **kargs)
+
+def clim(*args, **kargs):
+    _send_message('clim', *args, **kargs)
 
 def subplot(*args, **kargs):
     _send_message('subplot', *args, **kargs)
@@ -327,6 +486,12 @@ def plot(*args, **kargs):
     _send_message('plot', *args, **kargs)
 
 
+def scatter(*args, **kargs):
+    _send_message('scatter', *args, **kargs)
+
+def triplot(*args, **kargs):
+    _send_message('triplot', *args, **kargs)
+
 def errorbar(*args, **kargs):
     _send_message('errorbar', *args, **kargs)
 
@@ -334,9 +499,27 @@ def errorbar(*args, **kargs):
 def contour(*args, **kargs):
     _send_message('contour', *args, **kargs)
 
+def contourf(*args, **kargs):
+    _send_message('contourf', *args, **kargs)
+
+def quiver(*args, **kargs):
+    _send_message('quiver', *args, **kargs)
+
+def quiver3d(*args, **kargs):
+    _send_message('quiver3d', *args, **kargs)
+
 
 def image(*args, **kargs):
     _send_message('image', *args, **kargs)
+
+def tripcolor(*args, **kargs):
+    _send_message('tripcolor', *args, **kargs)
+
+def tripcontour(*args, **kargs):
+    _send_message('tricontour', *args, **kargs)
+
+def tripcontourf(*args, **kargs):
+    _send_message('tricontourf', *args, **kargs)
 
 
 def ispline(*args, **kargs):
@@ -354,14 +537,26 @@ def axspan(*args, **kargs):
 def text(*args, **kargs):
     _send_message('text', *args, **kargs)
 
+def figtext(*args, **kargs):
+    _send_message('figtext', *args, **kargs)
+
+def surf(*args, **kargs):
+    _send_message('surf', *args, **kargs)
+
+def surface(*args, **kargs):
+    _send_message('surface', *args, **kargs)
+
+def resolve(*args, **kargs):
+    _send_message('resolve', *args, **kargs)
+
+def solid(*args, **kargs):
+    _send_message('solid', *args, **kargs)
+
+def trisurf(*args, **kargs):
+    _send_message('trisurf', *args, **kargs)
+
 
 def legend(*args, **kargs):
     _send_message('legend', *args, **kargs)
 
-
-def get(*args, **kargs):
-    return _send_message_g('get_shellvar', *args, **kargs)
-
-
-def put(*args, **kargs):
-    return _send_message_g('pet_shellvar', *args, **kargs)
+'''
