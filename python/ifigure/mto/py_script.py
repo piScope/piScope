851a83f70a4e0422d65fb42774e36d409467304b
S. Shiraiwa <shiraiwa@princeton.edu>
2026-08-29 20:53:01 -0400

    fixec PyScript namespace for editor

--- python/ifigure/mto/py_script.py
+++ python/ifigure/mto/py_script.py
@@ -381,12 +381,44 @@ class PyScript(PyCode, FileHolder, AnsHolder):
         ns = super(PyScript, self).provide_ns_for_editor(editor=editor)
 
         ns['obj'] = self
+        ns['args'] = ()
+        ns['kwargs'] = {}
         try:
             tns = self.get_namespace()
             if isinstance(tns, dict):
                 ns.update(tns)
         except Exception:
             pass
+
+        top = None
+        try:
+            top = self.get_root_parent()
+        except Exception:
+            top = None
+
+        if top is not None:
+            ns['top'] = top
+            ns['proj'] = top
+            try:
+                ns['app'] = top.app
+            except Exception:
+                pass
+            try:
+                ns['wdir'] = top.getvar("wdir")
+            except Exception:
+                pass
+
+        # Runtime always injects control helpers and result callback symbols.
+        ns['stop'] = stop
+        ns['exit'] = exit
+
+        if 'ans' not in ns:
+            ns['ans'] = self.get_ansfunc()
+
+        if 'write_log' not in ns:
+            def _write_log(*args, **kwargs):
+                return None
+            ns['write_log'] = _write_log
 
         try:
             model = self.get_pymodel()
