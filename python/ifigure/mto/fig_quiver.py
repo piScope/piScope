cae0c5f1de987a2f67c26285b241d6aa2e8032ad
S. Shiraiwa <shiraiwa@princeton.edu>
2026-08-31 20:21:20 -0400

    removed unreachable segment

--- python/ifigure/mto/fig_quiver.py
+++ python/ifigure/mto/fig_quiver.py
@@ -418,24 +418,6 @@ class FigQuiver(FigObj, XUser, YUser, CUser):
             return crange
         return crange
 
-        ###
-        if (x is None or
-                y is None):
-            x = np.arange(z.shape[1])
-            y = np.arange(z.shape[0])
-
-        if (xrange[0] is not None and
-            xrange[1] is not None and
-            yrange[0] is not None and
-                yrange[1] is not None):
-            zt = np.ma.masked_array(z)
-            zt[(y < yrange[0]) | (y > yrange[1]), :] = np.ma.masked
-            zt[:, (x < xrange[0]) | (x > xrange[1])] = np.ma.masked
-            if scale == 'log':
-                zt[z <= 0] = np.ma.masked
-            crange = self._update_range(crange, (np.amin(zt), np.amax(zt)))
-        return crange
-
     def save_data2(self, data=None):
         def check(obj, name):
             if not isinstance(obj.getp(name), np.ndarray):
